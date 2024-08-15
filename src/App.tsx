aed589c35fb1af997881643490a47a84b5b64f34
Burak Şen <buraksen7@hotmail.com>
2024-08-15 10:18:49 +0200

    Fix plsss

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 
 const router = createHashRouter([
   {
-    path: "/blog/",
+    path: "/blog",
     element: (
       <div className="flex flex-col min-h-screen">
         <Navbar />
