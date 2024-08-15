134aa7f8a158fe0f411875cc06262c450b54f16d
Burak Şen <buraksen7@hotmail.com>
2024-08-15 10:45:23 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Blog from "@/pages/Blog";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Button } from "./components/ui/button";
+import { BiLeftArrow } from "react-icons/bi";
 
 const router = createHashRouter([
   {
@@ -23,7 +25,14 @@ const router = createHashRouter([
     element: (
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <div className="container w-full md:w-2/3 flex-grow p-4">
+        <div className="flex container w-full md:w-1/2 flex-grow p-4 flex-row">
+          <Button
+            variant="ghost"
+            className="w-1/12 hover:bg-slate-950 hover:text-lg hover:transition-all"
+            onClick={() => window.history.back()}
+          >
+            <BiLeftArrow />
+          </Button>
           <Blog />
         </div>
         <Footer />
