5e5dc86076b277d2e408046ff8199d1f3c2eb5b0
Burak Şen <buraksen7@hotmail.com>
2024-08-15 10:09:19 +0200

    Fix routing?

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/Home";
 import Blog from "@/pages/Blog";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/blog/",
     element: (
